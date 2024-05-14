5fac6391335175a8d9c2a28d0fab79375df9117c
Lei Ding <dinglei@kernelsoft.com>
2024-05-13 18:37:51 -0700

    sh : x64 暂不支持 gdb 调试

--- x86_64-2.6.34.sh
+++ x86_64-2.6.34.sh
@@ -6,10 +6,10 @@ LINUX_VERSION=linux-2.6.34
 source common.sh
 
 #  gdb 相关
-func["gdb-start"]="启动 gdb 服务"
-func["gdb-restart"]="重启 gdb 服务"
-func["gdb-stop"]="停止 gdb 服务"
-func["debug"]="连接 gdb 服务"
+# func["gdb-start"]="启动 gdb 服务"
+# func["gdb-restart"]="重启 gdb 服务"
+# func["gdb-stop"]="停止 gdb 服务"
+# func["debug"]="连接 gdb 服务"
 
 #
 # 调用主函数
